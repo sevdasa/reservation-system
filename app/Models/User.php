94bf5dc848c068bcd360b77877aa0ec4d6ea60eb
sevda <sevdasazdar@gmail.com>
2025-05-25 13:38:19 +0330

    chore: write relation for bookable

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function bookable()  {
+        return $this->hasOne(Bookable::class);
+    }
 }
